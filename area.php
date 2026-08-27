6c940eb9fe7feebf0a6f75b2dbe13fbde77e9c6f
Nicole Pinto <nicolepinto71@gmail.com>
2026-08-26 21:21:34 -0500

    Actualizo area

--- area.php
+++ area.php
@@ -1,13 +1,11 @@
 <?php
     $radio = (float) $_POST['radio'];
     $area = pi() * pow($radio, 2);
-    echo "El área de la circunferencia con radio $radio es: $area";
+    echo "El área de la circunferencia con radio $radio es: $area <br>";
 
     $radio = (float) $_POST['radio'];
 
     $area = pi() * pow($radio, 2);
     $perimetro = 2 * pi() * $radio;
-
-    echo "El área de la circunferencia con radio $radio es: $area <br>";
     echo "El perímetro de la circunferencia con radio $radio es: $perimetro";
 ?>

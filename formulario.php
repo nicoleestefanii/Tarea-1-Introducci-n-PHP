2d605bd610480ccbe0acfc8b51a1e197eb2714f1
Nicole Pinto <nicolepinto71@gmail.com>
2026-08-28 08:17:38 -0500

    Agrego formulario y pagina2

--- formulario.php
+++ formulario.php
@@ -5,23 +5,16 @@
 </head>
 
 <body>
-
-    <form method="post" action=" su nombre:
+    <form method="post" action="pagina2.php">
+        Ingrese su nombre:
         <input type="text" name="nombre" id="nombre">
-
         <br>
         <br>
-
         Ingrese su Edad:
         <input type="text" name="edad" id="edad">
-
-        <br>
-        <br>
-
+        <br><br>
         <input type="submit" value="confirmar">
-
     </form>
-
 </body>
 
 </html>
---
+++ pagina2.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombre = $_POST["nombre"];
+$edad = $_POST["edad"];
+
+echo "Su nombre es: " . $nombre;
+echo "<br>";
+echo "Su edad es: " . $edad;
+
+?>
